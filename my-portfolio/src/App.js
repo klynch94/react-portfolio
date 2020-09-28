0102d67a9474f8ed7d854a190a690301f7977dcb
Kieran Lynch <lynch.kieran1@gmail.com>
2020-09-28 18:29:56 -0500

    started to route pages and got bio/image on aboutMe page

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import Nav from "./components/Nav";
-import Portfolio from "./pages/Portfolio";
+// import Nav from "./components/Nav";
+// import Portfolio from "./pages/Portfolio";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
+// import Contact from "./pages/Contact";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer"
-import logo from './logo.svg';
+// import Header from "./components/Header";
+// import Footer from "./components/Footer"
 import './App.css';
 
 function App() {
   return (
-    
+    <Router>
+      {/* <Header/> */}
+      {/* <Nav/> */}
+      <Route exact path="/" component={About}/>
+      {/* <Route exact path="/portfolio" component={Portfolio}/> */}
+      {/* <Route exact path="/contact" component={Contact}/> */}
+      {/* <Footer/> */}
+    </Router>
   );
 }
 
---
+++ my-portfolio/src/components/Bio/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function () {
+    return (
+        <section className="container">
+            <div className="row">
+                <div className="col-sm-12">
+                    <div id="info" className="card mt-4">
+                        <div className="card-header">
+                            <h4>Kieran Lynch</h4>
+                        </div>
+                        <div className="card-body">
+                            <p className="about-me-text">
+                                <img src="./images/St.Andrews.Kieran.jpg" alt="Kieran Lynch" id="about-me-image" className="img-fluid"/>
+                                Howdy! My name is Kieran Lynch and I am currently based out of Chicago, Illinois. My professional experience includes bringing new products to market in the travel technology industry and driving software adoption. As of mid-2020, I am currently enrolled in a Full Stack Development program at Northwestern Univsersity and learning how to build my own full stack applications. Some of my interests include enjoying a good cup of tea, giving belly rubs to my Bernedoodle puppy - Murphy, soccer, and traveling.
+                            </p>
+                            <p>
+                                Check out some of my projects and let me know what you think...
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
