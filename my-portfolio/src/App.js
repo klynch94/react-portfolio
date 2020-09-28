3b8e0bdc8a3fde759889fb8708a823e063af1ebc
Kieran Lynch <lynch.kieran1@gmail.com>
2020-09-28 17:55:50 -0500

    created component and pages folder structure

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
+import Nav from "./components/Nav";
+import Portfolio from "./pages/Portfolio";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer"
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
   );
 }
 
