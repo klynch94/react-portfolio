b9f009f1c5ebd81d69cbdd1fec488bf9314c5748
Kieran Lynch <lynch.kieran1@gmail.com>
2020-09-28 21:33:20 -0500

    imported projects and rendered portfolio page successful

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Nav from "./components/Nav";
-// import Portfolio from "./pages/Portfolio";
+import Portfolio from "./pages/Portfolio";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <Nav/>
       <Route exact path="/" component={About}/>
-      {/* <Route exact path="/portfolio" component={Portfolio}/> */}
+      <Route exact path="/portfolio" component={Portfolio}/>
       <Route exact path="/contact" component={Contact}/>
       {/* <Footer/> */}
     </Router>
