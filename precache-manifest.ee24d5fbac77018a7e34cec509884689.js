30e014d67c9fa55bff8edc0f9df6b6fc7003a767
Kieran Lynch <lynch.kieran1@gmail.com>
2020-09-29 21:48:34 -0500

    Updates

---
+++ precache-manifest.ee24d5fbac77018a7e34cec509884689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76637bc704f7d1785225563e344a5072",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "16652b769bb1f79835cc",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "84d5bcc385ca8827b983",
+    "url": "/my-app/static/js/2.839069b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.839069b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16652b769bb1f79835cc",
+    "url": "/my-app/static/js/main.3a36d7a6.chunk.js"
+  },
+  {
+    "revision": "01bbda6f2f23ad0cf967",
+    "url": "/my-app/static/js/runtime-main.43a97011.js"
+  }
+]);
